1f707423bb937aaa75e730f5d01f5fb12406a67d
ssozinov <sergsozinov@gmail.com>
2018-02-07 11:39:10 +0300

    Function

--- testGradle/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ testGradle/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,18 @@ package ru.stqa.pft.sandbox;
 
 class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int b=4;
+        hello ("foo");
+        System.out.println(testReturn());
+    }
+
+    public static void hello (String s)
+    {
+        System.out.println(s);
+    }
+
+    public  static int  testReturn ()
+    {
+        return 5;
     }
 }
